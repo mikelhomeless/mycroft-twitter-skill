a3ad482cf9699671b3b080e92acb18400866d6b2
brihopki <brihopki@cisco.com>
2017-05-23 10:21:51 -0700

    adding in twitter_user to class

--- __init__.py
+++ __init__.py
@@ -40,7 +40,7 @@ LOGGER = getLogger(__name__)
 
 
 class TwitterAPI(object):
-    def __init__(self, consumer_key, consumer_secret, access_token, access_secret):
+    def __init__(self, consumer_key, consumer_secret, access_token, access_secret, twitter_user):
         auth = OAuthHandler(consumer_key, consumer_secret)
         auth.set_access_token(access_token, access_secret)
         self.api = tweepy.API(auth)
