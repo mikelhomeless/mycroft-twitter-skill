189e6f8489c239153b4ffe585bbbcec04ec23fd8
brihopki <brihopki@cisco.com>
2017-05-24 10:45:41 -0700

    adding join library

--- __init__.py
+++ __init__.py
@@ -23,7 +23,7 @@
 
 # Import statements: the list of outside modules you'll be using in your
 # skills, whether from other files in mycroft-core or from external libraries
-from os.path import dirname
+from os.path import dirname, join
 
 from adapt.intent import IntentBuilder
 from mycroft.skills.core import MycroftSkill
